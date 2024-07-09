08427e934d388cfd445812cdc89923a819c340cb
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-07-09 17:25:47 -0300

    Book Controller

---
+++ book_management/controller/BookController.js
@@ -0,0 +1,61 @@
+const Book = require("../models/Book");
+
+module.exports = class BookController {
+    static async showAllBooks(req, res) {
+        const tasks = await Book.findAll({raw: true});
+
+        res.render("task/all", {tasks});
+    }
+
+    static createBook(req, res) {
+        res.render("books/create");
+    }
+
+    static async addBook(req, res) {
+        const book = {
+            title: req.body.title,
+            category: req.body.category,
+            date: req.body.date,
+            author: req.body.author,
+            country: req.body.country,
+            description: req.body.description,
+        };
+
+        await Book.create(book);
+
+        res.redirect("book");
+    }
+
+    static async removeBook(req, res) {
+        const id = req.body.id;
+
+        await Book.destroy({where: {id}})
+
+        res.redirect("book")
+    }
+
+    static async updateBook(req, res) {
+        const id = req.params.id;
+
+        const book = await Book.findOne({where: {id}, raw: true});
+
+        res.render("task/edit", {task});
+    }
+
+    static async updateBookPost(req, res) {
+        const id = req.params.id;
+
+        const book = {
+            title: req.body.title,
+            category: req.body.category,
+            date: req.body.date,
+            author: req.body.author,
+            country: req.body.country,
+            description: req.body.description,
+        };
+
+        await Book.update({where: {id}});
+
+        res.render("/book");
+    }
+};
