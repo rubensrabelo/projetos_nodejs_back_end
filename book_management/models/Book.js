e775ceb3663bcc98f51496a307f9a127c3d5d3ca
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-07-09 17:03:11 -0300

    Book Model

--- book_management/models/Book.js
+++ book_management/models/Book.js
@@ -7,15 +7,21 @@ const Book = conn.define("Book", {
         type: DataTypes.STRING,
         require: true,
         unique: true,
+        allowNull: false,
     },
     category: {
-        type: DataTypes.STRING
+        type: DataTypes.STRING,
+        require: true,
+        allowNull: false,
     },
     date: {
         type: DataTypes.DATEONLY,
+        require: true,
+        allowNull: false,
     },
     author: {
-        type: DataTypes.STRING
+        type: DataTypes.STRING,
+        require: true,
     },
     country: {
         type: DataTypes.STRING
