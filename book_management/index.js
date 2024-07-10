0cfafd4d8255bf6bc19e47699bcaf1238848f8a9
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-07-10 12:27:17 -0300

    Editing index.js

--- book_management/index.js
+++ book_management/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 
 const conn = require("./db/conn");
+const Book = require("./models/Book");
+const booksRouters = require("./router/booksRouters");
 
 const app = express();
 
@@ -11,8 +13,12 @@ app.set("view engine", "handlebars");
 app.use(express.urlencoded({
     extended: true
 }));
+
 app.use(express.json());
+
 app.use(express.static("public"));
+
+app.use("/books", booksRouters);
 
 conn
 .sync()
