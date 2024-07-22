9260dbe7c3c8bb60d9316bba88713971871a10a1
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-07-22 16:19:25 -0300

    Initial configuration in the integration of authentication and user creation

--- book_management/index.js
+++ book_management/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const flash = require("express-flash");
 
 const conn = require("./db/conn");
+const Auth = require("./models/User");
 const Book = require("./models/Book");
+
+const authRoutes = require("./router/authRoutes");
 const booksRouters = require("./router/booksRouters");
+const BookController = require("./controller/BookController");
 
 const app = express();
 
@@ -16,11 +23,43 @@ app.use(express.urlencoded({
 
 app.use(express.json());
 
+//  Entender o funcionamento dessa parte
+app.use(
+    session({
+        name: "session",
+        secret: "my_secret", // Melhorar essa parte
+        resave: false,
+        saveUninitialized: false,
+        store: new FileStore({
+            logFn: function() {},
+            path: require("path").joint(require("os").tmpdir, "sessions"),
+        }),
+        cookie: {
+            secure: false,
+            maxAge: 360000,
+            expires: new Date(Date.now() + 360000),
+            httpOnly: true
+        }
+    }),
+);
+
+app.use(flash());
+
 app.use(express.static("public"));
+
+app.use((req, res, next) => {
+    if(req.session.userid) {
+        res.locals.session = req.session;
+    }
+});
 
 app.use("/books", booksRouters);
 
+app.use("/", authRoutes);
+app.get("/", BookController.showAllBooks);
+
 conn
+//.sync({force: true})
 .sync()
 .then(() => {
     app.listen(3000, () => {
